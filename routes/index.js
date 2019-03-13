44240160d74080d45206f835298c5cfe90f2cc55
mishramonalisha76 <mishramonalisha76@gmail.com>
2019-03-14 00:23:00 +0530

    UI made

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/signin', function(req, res) {
+  res.render('login');
+});
+router.get('/userprofile', function(req, res) {
+  res.render('profile');
+});
+router.get('/signup',function(req,res){
+  res.redirect("/userprofile");
+})
+router.get('/profile',function(req,res){
+  res.redirect("/userprofile");
+})
+router.get('/loginhere',function(req,res){
+  res.redirect("/signin");
+})
 
 module.exports = router;
